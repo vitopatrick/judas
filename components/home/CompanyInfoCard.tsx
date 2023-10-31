10945a877cfed8a005d1807bf57e7fa111fa3914
theWarlock <patrickvito2012@gmail.com>
2023-10-31 14:16:31 +0100

    fixed error

--- components/home/CompanyInfoCard.tsx
+++ components/home/CompanyInfoCard.tsx
@@ -1,19 +1,69 @@
-import { MiniChart } from "react-tradingview-embed";
+import { useEffect, useRef } from "react";
 
 type Details = {
   symbol: string;
 };
 
 const CompanyInfoCard = ({ symbol }: Details) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js";
+    script.async = true;
+
+    const options = {
+      symbol,
+      width: "100%",
+      height: "100%",
+      locale: "en",
+      dateRange: "12M",
+      colorTheme: "light",
+      isTransparent: false,
+      autosize: true,
+      largeChartUrl: "",
+    };
+
+    script.innerHTML = `
+      {
+        "symbol": "${options.symbol}",
+        "width": ${options.width},
+        "height": ${options.height},
+        "locale": "${options.locale}",
+        "dateRange": "${options.dateRange}",
+        "colorTheme": "${options.colorTheme}",
+        "isTransparent": ${options.isTransparent},
+        "autosize": ${options.autosize},
+        "largeChartUrl": "${options.largeChartUrl}"
+      }
+    `;
+
+    if (containerRef.current) {
+      containerRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeChild(script);
+      }
+    };
+  }, []);
+
   return (
-    <MiniChart
-      widgetProps={{
-        colorTheme: "light",
-        symbol: symbol,
-        width: "100%",
-        height: "100%",
-      }}
-    />
+    <div className="tradingview-widget-container" ref={containerRef}>
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
+    </div>
   );
 };
 
--- components/home/TickerTap.tsx
+++ components/home/TickerTap.tsx
@@ -1,14 +1,85 @@
-import { TickerTape } from "react-tradingview-embed";
+import { useEffect, useRef } from "react";
 
 type Props = {};
 
 const TickerTap = (props: Props) => {
+  const scriptRef = useRef<HTMLScriptElement | null>(null);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-tickers.js";
+    script.async = true;
+
+    const options = {
+      symbols: [
+        {
+          proName: "BITSTAMP:BTCUSD",
+          title: "Bitcoin",
+        },
+        {
+          proName: "BITSTAMP:ETHUSD",
+          title: "Ethereum",
+        },
+        {
+          description: "Cardano",
+          proName: "BINANCE:ADAUSDT",
+        },
+        {
+          description: "Litecoin",
+          proName: "BINANCE:LTCUSDT",
+        },
+        {
+          description: "Doge",
+          proName: "BINANCE:DOGEUSDT",
+        },
+      ],
+      colorTheme: "light",
+      isTransparent: false,
+      showSymbolLogo: true,
+      locale: "en",
+    };
+
+    script.innerHTML = `
+      {
+        "symbols": ${JSON.stringify(options.symbols)},
+        "colorTheme": "${options.colorTheme}",
+        "isTransparent": ${options.isTransparent},
+        "showSymbolLogo": ${options.showSymbolLogo},
+        "locale": "${options.locale}"
+      }
+    `;
+
+    scriptRef.current = script;
+
+    const widgetContainer = document.querySelector(
+      ".tradingview-widget-container__widget"
+    );
+    if (widgetContainer) {
+      widgetContainer.appendChild(script);
+    }
+
+    return () => {
+      if (scriptRef.current && scriptRef.current.parentNode) {
+        scriptRef.current.parentNode.removeChild(scriptRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <TickerTape
-      widgetProps={{
-        colorTheme: "light",
-      }}
-    />
+    <div className="tradingview-widget-container">
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a
+          href="https://www.tradingview.com/"
+          rel="noopener nofollow"
+          target="_blank"
+        >
+          <span className="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
+    </div>
   );
 };
 
